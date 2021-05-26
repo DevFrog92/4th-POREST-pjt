a214279a9427552301ac87fe2d6adc12690f04a4
yongsu kwon <yskwon0619@gmail.com>
2021-05-26 12:38:43 +0900

    fix : url

--- frontend/src/views/room/Approve.vue
+++ frontend/src/views/room/Approve.vue
@@ -158,11 +158,11 @@ export default {
       console.log('join');
       const form = document.querySelector('.approve-form');
       if (this.roomPublic) {
-        // form.action = 'https://live.porest.kr/';
-        form.action = 'http://localhost:3000/';
+        form.action = 'https://live.porest.kr/';
+        // form.action = 'http://localhost:3000/';
       } else {
-        // form.action = 'https://live.porest.kr/ar';
-        form.action = 'http://localhost:3000/ar';
+        form.action = 'https://live.porest.kr/ar';
+        // form.action = 'http://localhost:3000/ar';
       }
       form.method = 'POST';
       form.target = 'pop';
